d0297eec097130517151559b4b3d925fdbe30928
Nahuel Stella <nahuelprg@gmail.com>
2024-06-24 12:24:51 -0300

    Administración de Productos
    -Se hace el Listado de Productos
    -Se Crea un paginador
    -Formulario de Busqueda
    -Se muestran los resultados de la busqueda

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -15,3 +15,7 @@ export const OrderSchema  = z.object({
 export const OrderIdSchema = z.object({
     orderId: z.string().transform((value) => parseInt(value)).refine(value => value > 0, {message: "¡Error!"})
 });
+
+export const SearchSchema = z.object({
+    search: z.string().trim().min(1, {message: "La busqueda no puede ir vacia"})
+});
